a2fe349ffb05f9447d762b045a8aeee4e4afb2ad
H3c7o4 <hectorvladitok@gmail.com>
2023-07-06 12:45:28 +0200

    Fix bugs in 5-sum_list.py file

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,9 +6,10 @@ which takes a list input_list of floats as argument and
 returns their sum as a float.
 
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
 
     Args:
