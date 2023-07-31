05d9ece69fb01ec0ebbe71103a7a169c0acd8206
H3c7o4 <hectorvladitok@gmail.com>
2023-07-31 15:53:10 +0200

    create TestAccessNestedMap.test_access_nested_map_exception test for utils.access_nested_map function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,6 +21,14 @@ class TestAccessNestedMap(unittest.TestCase):
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
 
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+        ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
+
 
 if __name__ == '__main__':
     unittest.main()
